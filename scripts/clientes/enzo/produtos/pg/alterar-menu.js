d77176b3e06a5b9c73932e29a6549fa0eb83d95c
João Neto <joaoneto.20fsa@gmail.com>
2022-07-11 20:35:44 -0300

    Atualizações - 11/07/2022 - 20:35

--- scripts/clientes/enzo/produtos/pg/alterar-menu.js
+++ scripts/clientes/enzo/produtos/pg/alterar-menu.js
@@ -1,6 +1,6 @@
 (() => {
     // Remove item de vendas diretas, altera 'Endereços' para 'Concessionárias' e ajusta submenu 'Teste' para 'Corretora'
-    const navItems = document.querySelectorAll("header .nav-wrapper-dinamic .nav-wrapper-menu .nav > li");
+    let navItems = document.querySelectorAll("header .nav-wrapper-dinamic .nav-wrapper-menu .nav > li");
 
     navItems.forEach(navItem => {
         const navLink = navItem.querySelector("a");
@@ -38,5 +38,18 @@
                 <i class="fa fa-chevron-down down"></i>
             `;
         }
+    });
+
+    // Reordenar menu
+    const reorder = (before, after) => after.parentNode.insertBefore(before, after);
+    const insertAfter = (newNode, existingNode) => {
+        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    }
+
+    window.addEventListener("DOMContentLoaded", () => {
+        navItems = document.querySelectorAll("header .nav-wrapper-dinamic .nav-wrapper-menu .nav > li");
+
+        reorder(navItems[3], navItems[2]);
+        insertAfter(navItems[1], navItems[2]);
     });
 })();
